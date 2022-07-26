25c1fd0179708f871c3710287b6b61a19d7cba4e
iceman2146 <30576571+iceman2146@users.noreply.github.com>
2022-07-27 01:15:08 +0300

    01

--- Game.cpp
+++ Game.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
 
 #include "ZagadaiChislo.h"
-
+void GameLoop(int a, int b);
 int main() {
 
 
+    int target=ZagadayChislo();
+    int current_value=0;
 
-	//std::cout << ZagadayChislo() << std::endl;
+std::cout << target << std::endl;
+
+std::cout << "Enter your guess:" << std::endl;
+GameLoop(current_value,target);
+
 
 	return 0;
+}
+
+void GameLoop(int current_value, int target){
+    	do {
+		std::cin >> current_value;
+
+		if (current_value < target) {
+			std::cout << "Less " << current_value << std::endl;
+		}
+		else if (current_value > target) {
+			std::cout << "Greater " << current_value << std::endl;
+		}
+		else {
+			std::cout << "Win!" << std::endl;
+			break;
+		}
+
+	} while(true);
+
 }
