0b4ee12d691e1cb096ad07bcd3aace7e3283c6da
iceman2146 <30576571+iceman2146@users.noreply.github.com>
2022-07-27 23:36:15 +0300

    Gotovoe

--- Game.cpp
+++ Game.cpp
@@ -1,31 +1,45 @@
 #include <iostream>
+#include <string>
 
 #include "ZagadaiChislo.h"
-void GameLoop(int a, int b);
+#include "HighScore.h"
+void GameLoop(int a, int b,int&c);
+void Setup(int&);
+
+
 int main() {
-
-
-    int target=ZagadayChislo();
+    std::string user_name;
+	int attempt=0;
+    int target;
     int current_value=0;
+    std::cout << "Hi! Enter your name, please:" << std::endl;
+	std::cin >> user_name;
+
+    Setup(target);
+
 
 std::cout << target << std::endl;
 
-std::cout << "Enter your guess:" << std::endl;
-GameLoop(current_value,target);
-
-
-	return 0;
+std::cout <<user_name <<" enter your guess:" << std::endl;
+GameLoop(current_value,target, attempt);
+CreateandWriteinFile (user_name, attempt);
+ReadFile();
+	return 0; 
 }
 
-void GameLoop(int current_value, int target){
+void GameLoop(int current_value, int target, int& attempt)
+{
+	
     	do {
 		std::cin >> current_value;
 
 		if (current_value < target) {
 			std::cout << "Less " << current_value << std::endl;
+			attempt++;
 		}
 		else if (current_value > target) {
 			std::cout << "Greater " << current_value << std::endl;
+			attempt++;
 		}
 		else {
 			std::cout << "Win!" << std::endl;
@@ -34,4 +48,8 @@ void GameLoop(int current_value, int target){
 
 	} while(true);
 
+}
+void Setup(int &target)
+{
+    target=ZagadayChislo();
 }
